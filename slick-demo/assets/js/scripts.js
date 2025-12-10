99e9e70797aee9196ef6e4675c0a2b3084967d18
KalebM218 <ktmackie@ualr.edu>
2025-12-10 23:37:19 +0000

    slick demo

---
+++ slick-demo/assets/js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 1,   
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        speed: 500,
+        cssEase: 'linear'
+    });
+});
